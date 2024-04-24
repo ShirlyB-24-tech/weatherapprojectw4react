cf6b4925b672ca0314b8adc23d22067899e43a9a
ShirlyB-24-tech <160262117+ShirlyB-24-tech@users.noreply.github.com>
2024-04-24 16:01:26 +0200

    export folder

---
+++ fef87858-e3f5-4e02-a692-a9c52797e194/src/App.js
@@ -0,0 +1,12 @@
+import Search from "./Search";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search app</h1>
+      <Search />
+    </div>
+  );
+}
---
+++ fef87858-e3f5-4e02-a692-a9c52797e194/src/Search.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Search() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState(null);
+
+  function displayWeather(response) {
+    setLoaded(true);
+    console.log(response.data);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "a867e25f2d83db579421a57fd8e937ec";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="enter a city..."
+        onChange={updateCity}
+      />
+      <input type="submit" value="search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.humidity}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
